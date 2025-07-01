a97194de7db3d9a18d2a2da9b313cbbdd2535888
ATTACKER5520 <mashrabjonovabdujalil880@gmail.com>
2025-07-01 10:06:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const config = {
   version: '1.12',
   username: 'AT_OROL_1',
   password: 'abdu2006',
-  loginPassword: '1234444', // agar login komandasi boshqa parol bilan bo‘lsa
+  loginPassword: '', // agar login komandasi boshqa parol bilan bo‘lsa
   controller: 'ATTACKER'
 };
 
